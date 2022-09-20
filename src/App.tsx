67de0daefdb350427cef771f6cd144670814d118
Ty <ty@tiny.fun>
2022-09-20 14:18:48 +0700

    Router Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ProjectsPage from './projects/ProjectsPage';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ProjectsPage from "./projects/ProjectsPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import HomePage from "./home/HomePage";
 
 function App() {
   return (
-    <div className='container'>
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/projects" element={<ProjectsPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
