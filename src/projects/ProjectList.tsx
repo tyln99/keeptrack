cf7ad74cf9a9ecb45c371f500820a039245ad967
Ty <ty@tiny.fun>
2022-09-20 13:03:00 +0700

    - Split components - Add form

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Project } from "./Project";
+import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
   projects: Project[];
+  onSave: (project: Project) => void;
 }
 
-function ProjectList({ projects }: ProjectListProps) {
+function ProjectList({ projects, onSave }: ProjectListProps) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
+  const handleEdit = (project: Project) => {
+    setProjectBeingEdited(project);
+  };
+
+  const cancelEditing = () => {
+    setProjectBeingEdited({});
+  };
+
   return (
-    <pre>{JSON.stringify(projects, null, ' ')}</pre>
+    <div className="row">
+      {projects.map((project) => (
+        <div className="cols-sm">
+          {project === projectBeingEdited ? (
+            <ProjectForm
+              project={project}
+              onSave={onSave}
+              onCancel={cancelEditing}
+            />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
+        </div>
+      ))}
+    </div>
   );
 }
 
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
-import ProjectList from './ProjectList';
+import React, { useState } from "react";
+import { MOCK_PROJECTS } from "./MockProjects";
+import { Project } from "./Project";
+import ProjectList from "./ProjectList";
 
 function ProjectsPage() {
-    return (
-        <>
-            <h1>Projects</h1>
-            <ProjectList projects={MOCK_PROJECTS} />
-        </>
-    );
+  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+
+  const saveProject = (project: Project) => {
+    let updatedProjects = projects.map((p: Project) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
+  };
+
+  return (
+    <>
+      <h1>Projects</h1>
+      <ProjectList onSave={saveProject} projects={projects} />
+    </>
+  );
 }
 
 export default ProjectsPage;
