d3c5fc82061ded14cf78e7586fcfddbad91bee14
Ty <ty@tiny.fun>
2022-12-05 13:22:04 +0700

    Lab 24: Redux | Actions and Reducer

--- src/state.ts
+++ src/state.ts
@@ -2,24 +2,32 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
 
-const reducer = combineReducers({})
+import { ProjectState } from "./projects/state/projectTypes";
+import { initialProjectState } from "./projects/state/projectReducer";
+import { projectReducer } from "./projects/state/projectReducer";
+
+const reducer = combineReducers({
+  projectState: projectReducer,
+});
 
 export default function configureStore(preloadedState: any) {
-    const middlewares = [ReduxThunk]
-    const middlewareEnhancer = applyMiddleware(...middlewares)
-    //Thunk is middleware
-    //DevTools is an enhancer (actually changes Redux)
-    //applyMiddleware wraps middleware and returns an enhancer
+  const middlewares = [ReduxThunk];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  //Thunk is middleware
+  //DevTools is an enhancer (actually changes Redux)
+  //applyMiddleware wraps middleware and returns an enhancer
 
-    // to use only thunk middleware
-    // const enhancer = compose(middlewareEnhancer);
+  // to use only thunk middleware
+  // const enhancer = compose(middlewareEnhancer);
 
-    //to use thunk & devTools
-    const enhancer = composeWithDevTools(middlewareEnhancer);
-    const store = createStore(reducer, preloadedState, enhancer)
-    return store
+  //to use thunk & devTools
+  const enhancer = composeWithDevTools(middlewareEnhancer);
+  const store = createStore(reducer, preloadedState, enhancer);
+  return store;
 }
 
-export interface AppState {}
-export const initialAppState: AppState = {}
-export const store = configureStore (initialAppState)
+export interface AppState {
+  projectState: ProjectState;
+}
+export const initialAppState: AppState = { projectState: initialProjectState };
+export const store = configureStore(initialAppState);
